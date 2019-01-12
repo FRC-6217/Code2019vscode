684df4df88c3e6ec60e2a2b5b869b166c44388ac
stcoyle137 <22sc01@cf.k12.mn.us>
2019-01-12 08:37:29 -0600

    New changes that were changed on ecplise

--- src/main/java/org/usfirst/frc/team6217/robot/subsystems/SwerveDriveClass.java
+++ src/main/java/org/usfirst/frc/team6217/robot/subsystems/SwerveDriveClass.java
@@ -2,63 +2,74 @@ package org.usfirst.frc.team6217.robot.subsystems;
 
 //defining the class SwerveDrive;
 public class SwerveDriveClass {
-	
-	//creating constants for the width and length;
+
+	// creating constants for the width and length;
 	public final double L = 20.5;
 	public final double W = 14;
-	
+
 	private WheelDrive backRight;
 	private WheelDrive backLeft;
 	private WheelDrive frontRight;
 	private WheelDrive frontLeft;
-	
-	//creating a constructor called SwerveDriveClass defining its parameters;
-	public SwerveDriveClass (WheelDrive backRight, WheelDrive backLeft, WheelDrive frontRight, WheelDrive frontLeft) {
-	    this.backRight = backRight;
-	    this.backLeft = backLeft;
-	    this.frontRight = frontRight;
-	    this.frontLeft = frontLeft;
+
+	// creating a constructor called SwerveDriveClass defining its parameters;
+	public SwerveDriveClass(WheelDrive backRight, WheelDrive backLeft, WheelDrive frontRight, WheelDrive frontLeft) {
+		this.backRight = backRight;
+		this.backLeft = backLeft;
+		this.frontRight = frontRight;
+		this.frontLeft = frontLeft;
 	}
-	
-	public void drive (double x, double y, double z) {
-		/*First we need to find the radius of the circle the robot is going to spin
-		 * and for that we use the Pythagorean theorem and y *= -1 means y is assigned
-		 * the value of y * -1 */
-		double r = Math.sqrt ((L * L) + (W * W));
+
+	public void drive(double x, double y, double z) {
+		/*
+		 * First we need to find the radius of the circle the robot is going to spin and
+		 * for that we use the Pythagorean theorem and y *= -1 means y is assigned the
+		 * value of y * -1
+		 */
+		double r = Math.sqrt((L * L) + (W * W));
 		y *= -1;
 
-		//The next thing we do is assign the value a to the equation. a makes the robot go backwards
-		//Note: The L/r part makes the code in radians not degrees
-		
-		//a makes it go backwards;
-		double a = x - z * (L / r);
-		//b makes it go forwards.
-		double b = x + z * (L / r);
-		//c makes it go left.
-		double c = y - z * (W / r);
-		//d makes it go right.
-		double d = y + z * (W / r);
-	    
-		/*Now we do the Pythagorean theorem again because the robot will only go in a straight
-		 * line so we find the hypotenuse using our above variables for all direction combos. */
-		double backRightSpeed = Math.sqrt ((a * a) + (d * d));
-	        double backLeftSpeed = Math.sqrt ((a * a) + (c * c));
-	        double frontRightSpeed = Math.sqrt ((b * b) + (d * d));
-	        double frontLeftSpeed = Math.sqrt ((b * b) + (c * c));
+		// The next thing we do is assign the value a to the equation. a makes the robot
+		// go backwards
+		// Note: The L/r part makes the code in radians not degrees
 
-	        /*In order to find the angles we need to turn based on the inputs of the controller
-	         * the code turns the tangent of the coordinates (x,y) into (radius,angle) to find the
-	         * angle that applies to our robot. Then it's divided by pi to turn from radians 
-	         * into degrees. */
-	        double backRightAngle = Math.atan2 (a, d) / Math.PI;
-	        double backLeftAngle = Math.atan2 (a, c) / Math.PI;
-	        double frontRightAngle = Math.atan2 (b, d) / Math.PI;
-	        double frontLeftAngle = Math.atan2 (b, c) / Math.PI;
-	    
-	        /*Lastly the results of the above code are all plugged back in to be used later. */
-	        backRight.drive (backRightSpeed, backRightAngle);
-	        backLeft.drive (backLeftSpeed, backLeftAngle);
-	        frontRight.drive (frontRightSpeed, frontRightAngle);
-	        frontLeft.drive (frontLeftSpeed, frontLeftAngle);
+		// a makes it go backwards;
+		double a = x - z * (L / r);
+		// b makes it go forwards.
+		double b = x + z * (L / r);
+		// c makes it go left.
+		double c = y - z * (W / r);
+		// d makes it go right.
+		double d = y + z * (W / r);
+
+		/*
+		 * Now we do the Pythagorean theorem again because the robot will only go in a
+		 * straight line so we find the hypotenuse using our above variables for all
+		 * direction combos.
+		 */
+		double backRightSpeed = Math.sqrt((a * a) + (d * d));
+		double backLeftSpeed = Math.sqrt((a * a) + (c * c));
+		double frontRightSpeed = Math.sqrt((b * b) + (d * d));
+		double frontLeftSpeed = Math.sqrt((b * b) + (c * c));
+
+		/*
+		 * In order to find the angles we need to turn based on the inputs of the
+		 * controller the code turns the tangent of the coordinates (x,y) into
+		 * (radius,angle) to find the angle that applies to our robot. Then it's divided
+		 * by pi to turn from radians into degrees.
+		 */
+		double backRightAngle = Math.atan2(a, d) / Math.PI;
+		double backLeftAngle = Math.atan2(a, c) / Math.PI;
+		double frontRightAngle = Math.atan2(b, d) / Math.PI;
+		double frontLeftAngle = Math.atan2(b, c) / Math.PI;
+
+		/*
+		 * Lastly the results of the above code are all plugged back in to be used
+		 * later.
+		 */
+		backRight.drive(backRightSpeed, backRightAngle - 4.360, false);
+		backLeft.drive(backLeftSpeed, backLeftAngle - 1.255, true);
+		frontRight.drive(frontRightSpeed, frontRightAngle - 0.493, false);
+		frontLeft.drive(frontLeftSpeed, frontLeftAngle - 3.250, true);
 	}
 }
--- src/main/java/org/usfirst/frc/team6217/robot/subsystems/WheelDrive.java
+++ src/main/java/org/usfirst/frc/team6217/robot/subsystems/WheelDrive.java
@@ -4,46 +4,43 @@ import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 
 import edu.wpi.first.wpilibj.AnalogInput;
-import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.PIDController;
-import edu.wpi.first.wpilibj.PIDOutput;
 
 public class WheelDrive {
 	private VictorSPX angleMotor;
 	private VictorSPX speedMotor;
 	private VictorSPX_PIDOutput motorPID;
 	private PIDController pidController;
-	private Encoder enc;
-	private final double MAX_VOLTS = 7;
-	
-	public WheelDrive (int angleMotor, int speedMotor, int encoder) {
-	    this.angleMotor = new VictorSPX (angleMotor);
-	    this.speedMotor = new VictorSPX (speedMotor);
-	    this.motorPID = new VictorSPX_PIDOutput (this.angleMotor);
-	    this.enc = new Encoder(encoder, encoder + 1);
-	    //VictorSPX is not a subclass of PIDOutput;
-	    pidController = new PIDController (1, 0, 0, enc, this.motorPID);
+	private final double MAX_VOLTS = 5;
 
-	    pidController.setOutputRange (-1, 1);
-	    pidController.setContinuous ();
-	    pidController.enable ();
-	    
+	public WheelDrive(int angleMotor, int speedMotor, int encoder) {
+		this.angleMotor = new VictorSPX(angleMotor);
+		this.speedMotor = new VictorSPX(speedMotor);
+		this.motorPID = new VictorSPX_PIDOutput(this.angleMotor);
+		// VictorSPX is not a subclass of PIDOutput;
+		pidController = new PIDController(1, 0, 0, new AnalogInput(encoder), this.motorPID);
+
+		pidController.setInputRange(0, MAX_VOLTS);
+		pidController.setOutputRange(-1, 1);
+		pidController.setContinuous();
+		pidController.enable();
+
 	}
-	
-	public void drive (double speed, double angle) {
-	    speedMotor.set(ControlMode.PercentOutput, speed);
-	    
-	    double setpoint = (angle * (MAX_VOLTS * 0.5)) + (MAX_VOLTS * 0.5); // Optimization offset can be calculated here.
-	    if (setpoint < 0) {
-	        setpoint = MAX_VOLTS + setpoint;
-	    }
-	    if (setpoint > MAX_VOLTS) {
-	        setpoint = setpoint - MAX_VOLTS;
-	    }
 
-	    pidController.setSetpoint (setpoint);
+	public void drive(double speed, double angle, boolean isInverted) {
+		// if(isInverted = true) {speed *= -1;};
+		speedMotor.set(ControlMode.PercentOutput, -speed);
+
+		double setpoint = (angle * (MAX_VOLTS * 0.5)) + (MAX_VOLTS * 0.5); // Optimization offset can be calculated
+																			// here.
+		if (setpoint < 0) {
+			setpoint = MAX_VOLTS + setpoint;
+		}
+		if (setpoint > MAX_VOLTS) {
+			setpoint = setpoint - MAX_VOLTS;
+		}
+
+		pidController.setSetpoint(setpoint);
 	}
-	
 
-	
 }
